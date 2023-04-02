eb02c0c3606b6cfa7d25a0a6c7d1e2ae0f3a5673
Mickon <jtandoh010@st.ug.edu.gh>
2023-04-02 00:38:35 +0000

    Disabled sidebar tweet button when user is logged in

--- components/Layouts/SideBarTweetButton.tsx
+++ components/Layouts/SideBarTweetButton.tsx
@@ -3,60 +3,91 @@ import { FaFeather } from 'react-icons/fa'
 import {  useCallback} from 'react'
 import useLoginModel from '@/hooks/useLoginModel';
 
+import { getSession } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
+// import {goToPost} from '../posts/PostItem'
+
 
 
 const SideBarTweetButton = () => {
  const router = useRouter();
  const loginModel = useLoginModel();
+ const { data: currentUser } = useCurrentUser();
 
- const onClick = useCallback(()=> {
-	loginModel.onOpen();
+
+//  const goToPost = useCallback(()=>{
+// 	router.push(`/posts/${data.id}`);
+// },[router, data.id]);  //Trying to add an onclick to the tweet button 
+//that would eould direct user to login page if they are not logged in
+// else direct them to tweet page
+ 
+ 
+
+ const onClick = useCallback(async()=> {
+	const currentUser = await getSession();
+	try {
+		if(!currentUser){
+			loginModel.onOpen();
+		};
+
+		// if(currentUser){
+		// 	goToPost
+		// }
+		
+	} catch (error) {
+		console.log(error);	
+	}
+	
+	
  },[loginModel])
 
 	
 	return (
 		<div onClick={onClick}>
-			<div className='
-								mt-6
-								lg-hidden
-								rounded-full
-								h-14
-								w-14
-								p-4
-								items-center
-								justify-center
-								bg-sky-500
-								hover:bg-opacity-80
-								transition
-								cursor-pointer
+			<div 
+				className='
+					mt-6
+					lg-hidden
+					rounded-full
+					h-14
+					w-14
+					p-4
+					items-center
+					justify-center
+					bg-sky-500
+					hover:bg-opacity-80
+					transition
+					cursor-pointer
 
-							'
+				'
 			>
 				<FaFeather size={24} color = 'white' />
 
 			</div>
-			<div className='
-								mt-6
-								hidden
-								lg:block
-								px-4
-								py-2
-								rounded-full
-								bg-sky-500
-								hover:bg-opacity-90
-								cursor-pointer
-								transition
+			<div 
+				className='
+					mt-6
+					hidden
+					lg:block
+					px-4
+					py-2
+					rounded-full
+					bg-sky-500
+					hover:bg-opacity-90
+					cursor-pointer
+					transition
 
-							'
-			>
-				<p className='
-							hidden 
-							lg:block 
-							text-center 
-							font-semibold 
-						text-white 
-							text-[20px]
 				'
+			>
+				<p 
+					className='
+						hidden 
+						lg:block 
+						text-center 
+						font-semibold 
+					text-white 
+						text-[20px]
+					'
 				>
 					Tweet
 				</p>
